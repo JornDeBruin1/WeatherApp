36fd037606842e504395eef9a04d11adda35267b
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2023-11-10 13:30:42 +0100

    you can now click on an state/city

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -26,6 +26,7 @@
             v-for="searchResult in mapboxSearchResults"
             :key="searchResult.id"
             class="py-2 cursor-pointer" 
+            @click="previewCity(searchResult)"
           >
             {{ searchResult.place_name }}
           </li>
@@ -38,6 +39,23 @@
 <script setup>
 import { ref } from 'vue';
 import axios from 'axios'
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+const previewCity = (searchResult) => {
+  console.log(searchResult)
+  const [city, state] = searchResult.place_name.split(",");
+  router.push({
+    name: "cityView",
+    params: { state: state.replaceAll(" ", ""), city: city },
+    query:{
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true,
+    }
+  })
+  
+}
 
 const mapBoxAPIKey = "pk.eyJ1Ijoiam9ybmRlYnJ1aW4iLCJhIjoiY2xvc2hjcTdkMDBoNzJqcTA4N245eW5neSJ9.3LX7ZM3tTNpulvjiJoGajg"
 const searchQuery = ref("");
