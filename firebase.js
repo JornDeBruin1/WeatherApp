0c4102bfdff4afc95ca754294549a57562875e2d
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2023-11-15 17:07:15 +0100

    firebase connection

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+import { ref, onUnmounted } from 'vue'
+
+const config = {
+    apiKey: "AIzaSyBIIfRelpWkHS0re_7F16rbrHRGh_3nyw4",
+    authDomain: "weather-app-6d8f2.firebaseapp.com",
+    projectId: "weather-app-6d8f2",
+    storageBucket: "weather-app-6d8f2.appspot.com",
+    messagingSenderId: "525164970038",
+    appId: "1:525164970038:web:d2c991708329f00b031897"
+  };
+  
+  const firebaseApp = firebaseApp.initializeApp(config);
