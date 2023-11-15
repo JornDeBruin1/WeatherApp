b8530640427a6e633f7f32604ee3883b8f1a5eb7
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2023-11-15 14:47:51 +0100

    routing and title changes

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
   <div class="flex flex-col min-h-screen font-Roboto
   bg-weather-primary">
     <SiteNavigation />    
-    <RouterView />
+    <RouterView v-slot="{ Component }">
+      <Transition name="page" mode="out-in">
+        <component :is="Component" />
+      </Transition>
+    </RouterView>
   </div>
 </template>
 
@@ -11,6 +15,12 @@
   import SiteNavigation from './components/SiteNavigation.vue'
 </script>
 
-<style scoped>
+<style>
+  .page-enter-active, .page-leave-active {
+    transition: 600ms ease all;
+  }
 
+  .page-enter-from, .page-leave-to {
+    opacity: 0;
+  }
 </style>
