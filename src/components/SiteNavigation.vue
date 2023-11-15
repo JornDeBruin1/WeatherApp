6fa5560e1429b2dc7c188cdfee3b22dd216c0084
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2023-11-15 10:21:39 +0100

    cities can be saved to local storage

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -16,7 +16,10 @@
                 </i>
                 <i class="fa-solid fa-plus text-xl
                  hover:text-weather-secondary duration-150
-                 cursor-pointer"></i>
+                 cursor-pointer"
+                 @click="addCity()"
+                 v-if="route.query.preview"
+                 ></i>
             </div>
             <BaseModal 
                 :modalActive="modalActive"
@@ -61,8 +64,40 @@
 
 <script setup>
 import { ref } from 'vue';
-import { RouterLink } from 'vue-router';
-import BaseModal from './BaseModal.vue'
+import { uid} from 'uid';
+import { RouterLink, useRouter, useRoute } from 'vue-router';
+import BaseModal from './BaseModal.vue';
+
+const savedCities = ref([])
+const route = useRoute();
+const router = useRouter();
+const addCity = () => {
+    if(localStorage.getItem('savedCities')){
+        savedCities.value = JSON.parse(
+            localStorage.getItem('savedCities')
+        );
+    }
+
+    const locationObj = {
+        id: uid(),
+        state: route.params.state,
+        city: route.params.city,
+        coords: {
+            lat: route.query.lat,
+            lng: route.query.lng,
+        },
+    };
+
+    savedCities.value.push(locationObj);
+    localStorage.setItem(
+        'savedCities', 
+        JSON.stringify(savedCities.value) 
+    );
+
+    let query = Object.assign({}, route.query);
+    delete query.preview;
+    router.replace({ query })
+}
 
 const modalActive = ref(null);
 const toggleModal = () => {
