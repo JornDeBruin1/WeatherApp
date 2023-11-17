b9211ff0583cd62fc57a06f99b3a5d7ef11c846b
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2023-11-17 09:16:27 +0100

    pathing for main.js was wrong fix

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
-import { initGoogleAnalytics } from './services/googleAnalyticsService';
+import { initGoogleAnalytics } from './services/googleAnalyticService';
 
 
 initGoogleAnalytics();
