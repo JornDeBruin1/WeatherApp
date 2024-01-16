c54dff7e3ff0469bbc4ff53447da913e82787061
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2024-01-16 17:06:19 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 module.exports = {
-	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-	theme: {
-		extend: {
-			colors: {
-				'weather-primary': '#00668A',
-				'weather-secondary': '#004E71',
-				'weather-tertiary': '#037ba3',
-			},
-		},
-		fontFamily: {
-			Roboto: ['Roboto, sans-serif'],
-		},
-		container: {
-			padding: '2rem',
-			center: true,
-		},
-		screens: {
-			sm: '640px',
-			md: '768px',
-		},
-	},
-	plugins: [require('tailwind-scrollbar')],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'weather-primary': '#00668A',
+        'weather-secondary': '#004E71',
+        'weather-tertiary': '#037ba3',
+      },
+    },
+    fontFamily: {
+      Roboto: ['Roboto, sans-serif'],
+    },
+    container: {
+      padding: '2rem',
+      center: true,
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+    },
+  },
+  plugins: [require('tailwind-scrollbar')],
 };
